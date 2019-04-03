837d119af9b3b5276f70caa5d7cb22a5cdffa0a9
Johan Niklasson <johan@nicklassons.se>
2019-04-03 15:17:12 +0200

    Added eigenvalues matrix

--- src/GraphSolver/GraphSolver.py
+++ src/GraphSolver/GraphSolver.py
@@ -108,6 +108,58 @@ class GraphSolver:
             "graphs": graph_list
         }
 
+    @staticmethod
+    def get_neighbour_matrix(g):
+        """
+
+        :type g: networkx
+        """
+        # Sort the nodes in the graph
+        s_nodes = list(g.nodes())
+        s_nodes.sort()
+        # Get the dimension of each row
+        dim = len(s_nodes)
+
+        # Create an empty row
+        row = [0] * dim
+        A = []
+        for node in g.nodes():
+            row = [0] * dim
+            # Get the index of the current node
+            node_index = s_nodes.index(node)
+            row[node_index] = 1
+            # Get the number of neighbours
+            neighbour_count = 0
+            for neighbour in g.neighbors(node):
+                node_index = s_nodes.index(neighbour)
+                row[node_index] = 1
+                neighbour_count += 1
+            row_divide = float(neighbour_count + 1)
+            row = map(lambda x: x/row_divide, row)
+            A.append(row)
+        return A
+
+    @staticmethod
+    def get_eigenvalues(a):
+        import numpy as np
+        from numpy import linalg as LA
+        A = np.array(a)
+        w, _ = LA.eig(A)
+        return w
+
+    @staticmethod
+    def second_largest(numbers):
+        count = 0
+        m1 = m2 = float('-inf')
+        for x in numbers:
+            count += 1
+            if x > m2:
+                if x >= m1:
+                    m1, m2 = x, m1
+                else:
+                    m2 = x
+        return m2 if count >= 2 else None
+
     def consensus_average(self, g):
         graph = copy.deepcopy(g)
 
