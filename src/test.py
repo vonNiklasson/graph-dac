a96f3d9d97e898b7d4b6addcf4c60cfff72c8e5d
Johan Niklasson <johan@nicklassons.se>
2019-04-04 12:43:07 +0200

    Implemented new solver in test.py

--- src/test.py
+++ src/test.py
@@ -2,17 +2,17 @@ from GraphConverter import GraphConverter as gc
 from GraphSolver import GraphSolver
 from GraphPrinter import GraphPrinter
 
-from graphs import square
+from graphs import hossein
 
-g = gc.from_dict(square)
+g = gc.from_dict(hossein)
 
 gs = GraphSolver()
 
-y = gs.solve(g, 6)
+y = gs.solve(g, 8)
 
 for edge_count, graph in y['graphs'].items():
     if graph is not None:
-        print "Graph with " + str(edge_count) + " edges"
+        print "Graph with " + str(edge_count) + " edges: " + str(y['convergence_rate'][edge_count])
         GraphPrinter.draw(graph)
         raw_input("Press enter to draw next graph")
 
